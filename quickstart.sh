8acf64698627ec7a5918bab5fe30c61834e644a3
Jett-Zhang <yfszhj@163.com>
2025-06-15 15:30:57 +0800

    no message

--- quickstart.sh
+++ quickstart.sh
@@ -3,20 +3,20 @@ set -e
 
 # 你所有脚本的文件名
 files=(
-    gost_docker_manager.sh
+    docker_gost_manager.sh
 )
 
 # 创建目标目录
-mkdir -p gost_docker_manager/sh
+mkdir -p docker_gost_manager/sh
 
 # 下载所有脚本
 for file in "${files[@]}"; do
     # echo "正在下载 $file ..."
-    curl -fsSL -o "gost_docker_manager/sh/$file" "https://raw.githubusercontent.com/imjettzhang/gost_docker_manager/main/$file"
+    curl -fsSL -o "docker_gost_manager/sh/$file" "https://raw.githubusercontent.com/imjettzhang/docker_gost_manager/main/$file"
 done
 
 # 给所有脚本加执行权限
-chmod +x gost_docker_manager/sh/*.sh
+chmod +x docker_gost_manager/sh/*.sh
 
 # 运行主程序
-./gost_docker_manager/sh/gost_docker_manager.sh
+./docker_gost_manager/sh/docker_gost_manager.sh
