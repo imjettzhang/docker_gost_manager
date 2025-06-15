2a7024860912c9cf05503503ff87bc8d30c74d9e
Jett-Zhang <yfszhj@163.com>
2025-06-15 15:26:28 +0800

    no message

---
+++ quickstart.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# 你所有脚本的文件名
+files=(
+    gost_docker_manager.sh
+)
+
+# 创建目标目录
+mkdir -p gost_docker_manager/sh
+
+# 下载所有脚本
+for file in "${files[@]}"; do
+    # echo "正在下载 $file ..."
+    curl -fsSL -o "gost_docker_manager/sh/$file" "https://raw.githubusercontent.com/imjettzhang/gost_docker_manager/main/$file"
+done
+
+# 给所有脚本加执行权限
+chmod +x gost_docker_manager/sh/*.sh
+
+# 运行主程序
+./gost_docker_manager/sh/gost_docker_manager.sh
